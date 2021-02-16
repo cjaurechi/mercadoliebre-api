85c3646d338b253e28ad62389f177bfe62ba24b1
cjaurechi <cjaurechi@asapp.com>
2021-02-16 15:13:03 -0300

    Estandarizacion de nombre de controladores

--- src/routes/api/productsRouter.js
+++ src/routes/api/productsRouter.js
@@ -4,12 +4,12 @@ const router = express.Router();
 
 // ************ Controller Require ************
 
-const apiProductsController = require('../../controllers/api/productsController');
+const productsControllerAPI = require('../../controllers/api/productsController');
 
 // ************       Routes       ************
 
-router.get('/latest', apiProductsController.latest); /* GET - Latest products */
-router.get('/offers', apiProductsController.offers); /* GET - Offers */
-router.get('/:category?', apiProductsController.getProducts); /* GET - All products from one specific category */
+router.get('/latest', productsControllerAPI.latest); /* GET - Latest products */
+router.get('/offers', productsControllerAPI.offers); /* GET - Offers */
+router.get('/:category?', productsControllerAPI.getProducts); /* GET - All products from one specific category */
 
 module.exports = router
