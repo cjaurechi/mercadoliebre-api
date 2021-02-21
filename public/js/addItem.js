f42b3abd06458b354edf75f0de949cef77d52ac4
cjaurechi <cjaurechi@asapp.com>
2021-02-21 20:35:05 -0300

    Corrección de errores en JS front (Add item to cart y Products)

--- public/js/addItem.js
+++ public/js/addItem.js
@@ -1,30 +1,30 @@
 window.addEventListener('load', function () {
     let addForm = document.getElementById('addForm');
-    let quantityInput = document.getElementById('quantity');
-    let productId = addForm.productId.value;
+    if (addForm) {
+        let quantityInput = document.getElementById('quantity');
+        let productId = addForm.productId.value;
 
-    addForm.addEventListener('submit', function (event) {
-        event.preventDefault();
-        let quantityValue = quantityInput.value;
-        axios({
-                method: 'post',
-                url: 'http://localhost:3000/api/items',
-                data: {
-                    quantity: quantityValue,
-                    productId: productId
-                }
-            })
-            .then(response => {
-                if (response.status == '200') {
-                    window.location.assign('http://localhost:3000/users/cart') // Redirigir al Carrito desde el FE?
-                } else {
-                    console.log('Error?');
-                }
-            })
-            .catch(errors => {
-                console.log(errors);
-            })
-    })
+        addForm.addEventListener('submit', function (event) {
+            event.preventDefault();
+            let quantityValue = quantityInput.value;
+            axios({
+                    method: 'post',
+                    url: 'http://localhost:3000/api/items',
+                    data: {
+                        quantity: quantityValue,
+                        productId: productId
+                    }
+                })
+                .then(response => {
+                    if (response.status == '200') {
+                        window.location.assign('http://localhost:3000/users/cart')
+                    } else {
+                        console.log('Error?');
+                    }
+                })
+                .catch(errors => {
+                    console.log(errors);
+                })
+        })
+    }
 })
-
-// FALTAN AGREGAR MIDDLEWARES Y VALIDACIONES!!
--- public/js/products.js
+++ public/js/products.js
@@ -2,12 +2,14 @@ axios('http://localhost:3000/api/products/latest')
     .then(response => {
         let productos = response.data.data;
         let ultimosProductos = document.getElementById('latest-products')
-        for(let i = 0; i < ultimosProductos.childElementCount; i++) {
+        for (let i = 0; i < ultimosProductos.childElementCount; i++) {
             ultimosProductos.children[i].querySelector('a').href = "/products/detail/" + productos[i].id;
             ultimosProductos.children[i].querySelector('img').src = "/images/products/" + productos[i].image;
             ultimosProductos.children[i].querySelector('img').alt = productos[i].name;
             ultimosProductos.children[i].querySelector('h2').innerText = "$ " + Math.trunc(productos[i].price - productos[i].price * productos[i].discount / 100);
-            ultimosProductos.children[i].querySelector('span').innerText = productos[i].discount + " % OFF";
+            if (ultimosProductos.children[i].querySelector('span')) {
+                ultimosProductos.children[i].querySelector('span').innerText = productos[i].discount + " % OFF";
+            }
             ultimosProductos.children[i].querySelector('p').innerText = productos[i].name
         }
     })
@@ -18,12 +20,14 @@ axios('http://localhost:3000/api/products/offers')
     .then(response => {
         let productos = response.data.data;
         let productosEnOferta = document.getElementById('offers-products')
-        for(let i = 0; i < productosEnOferta.childElementCount; i++) {
+        for (let i = 0; i < productosEnOferta.childElementCount; i++) {
             productosEnOferta.children[i].querySelector('a').href = "/products/detail/" + productos[i].id;
             productosEnOferta.children[i].querySelector('img').src = "/images/products/" + productos[i].image;
             productosEnOferta.children[i].querySelector('img').alt = productos[i].name;
             productosEnOferta.children[i].querySelector('h2').innerText = "$ " + Math.trunc(productos[i].price - productos[i].price * productos[i].discount / 100);
-            productosEnOferta.children[i].querySelector('span').innerText = productos[i].discount + " % OFF";
+            if (productosEnOferta.children[i].querySelector('span')) {
+                productosEnOferta.children[i].querySelector('span').innerText = productos[i].discount + " % OFF";
+            }
             productosEnOferta.children[i].querySelector('p').innerText = productos[i].name
         }
     })
